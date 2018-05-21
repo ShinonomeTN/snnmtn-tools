1e4e0a71dd34dcd3dc4beb4208666118c6c6c47d
CattenLinger <lingerkong03@gmail.com>
2018-05-21 21:48:23 +0800

    v1.1-SNAPSHOT - Add SHA512

--- src/main/java/com/shinonometn/commons/tools/Digests.java
+++ src/main/java/com/shinonometn/commons/tools/Digests.java
@@ -7,6 +7,22 @@ import java.security.NoSuchAlgorithmException;
 
 public final class Digests {
 
+    private final static MessageDigest md5Digest;
+    private final static MessageDigest sha1Digest;
+
+    private final static MessageDigest sha512Digest;
+
+    static {
+        try {
+            md5Digest = MessageDigest.getInstance("MD5");
+            sha1Digest = MessageDigest.getInstance("SHA-1");
+
+            sha512Digest = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     /**
      *
      * Get MD5 of some strings
@@ -17,12 +33,11 @@ public final class Digests {
      * @throws NoSuchAlgorithmException
      */
     public static String getMD5(boolean upperCase, String... strings) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
 
         StringBuilder stringBuilder = new StringBuilder();
         for (String s : strings) stringBuilder.append(s);
 
-        return new String(Hex.encodeHex(messageDigest.digest(stringBuilder.toString().getBytes()), !upperCase));
+        return new String(Hex.encodeHex(md5Digest.digest(stringBuilder.toString().getBytes()), !upperCase));
     }
 
     /**
@@ -32,14 +47,30 @@ public final class Digests {
      * @param upperCase if use uppercase string as result
      * @param strings contents
      * @return string
-     * @throws NoSuchAlgorithmException
      */
-    public static String getSha1(boolean upperCase, String... strings) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+    public static String getSha1(boolean upperCase, String... strings) {
+
 
         StringBuilder stringBuilder = new StringBuilder();
         for (String s : strings) stringBuilder.append(s);
 
-        return new String(Hex.encodeHex(messageDigest.digest(stringBuilder.toString().getBytes()), !upperCase));
+        return new String(Hex.encodeHex(sha1Digest.digest(stringBuilder.toString().getBytes()), !upperCase));
+    }
+
+    /**
+     *
+     * Get SHA512 of some string
+     *
+     * @param upperCase if use uppercase string as result
+     * @param strings contents
+     * @return string
+     */
+    public static String getSha512(boolean upperCase, String... strings) {
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : strings) stringBuilder.append(s);
+
+        return new String(Hex.encodeHex(sha512Digest.digest(stringBuilder.toString().getBytes()), !upperCase));
     }
 }
