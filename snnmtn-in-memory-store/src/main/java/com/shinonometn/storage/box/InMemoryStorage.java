b681e0fcc5b73fc43f87ceb9abbcdaedf2199d3c
CattenLinger <lingerkong03@gmail.com>
2018-12-16 15:06:16 +0800

    Add InMemoryStore module

---
+++ snnmtn-in-memory-store/src/main/java/com/shinonometn/storage/box/InMemoryStorage.java
@@ -0,0 +1,42 @@
+package com.shinonometn.storage.box;
+
+/**
+ *
+ * InMemoryStorage interface, provides some method to manage instances
+ *
+ * InMemoryStorage is a concept for managing some global information in CMS systems that
+ * need to be update and change frequently.
+ *
+ * It is using in ReSSIM course data system for storing and managing term info, visitor statistics,
+ * school calendar and other global information.
+ *
+ * It recommenced to inherit the InMemoryStorageAdapter for usage
+ *
+ */
+public interface InMemoryStorage {
+
+    /**
+     *
+     * Provide a domain name to manage those storage
+     *
+     * @return string
+     */
+    String domain();
+
+    /**
+     * Clear all data
+     */
+    void clear();
+
+    /**
+     *
+     * Reset the storage to it's initial state
+     *
+     * default is clear all data
+     *
+     */
+    default void reset(){
+        clear();
+    }
+
+}
